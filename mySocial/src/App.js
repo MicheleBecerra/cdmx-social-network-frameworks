46688b2cc72fb6076359df86e4365adb86457429
michele velazquez de la rosa becerra <michbecerra@hotmail.com>
2018-09-21 12:42:35 -0500

    Agregando funcionalidad del LogIn conGoogle

---
+++ mySocial/src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+import './App.css'
+// import Navigation from './components/Navigation'
+import firebase from 'firebase'
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      user: null
+    }
+
+    this.handleAuth = this.handleAuth.bind(this)
+    this.handleLogout = this.handleLogout.bind(this)
+  }
+
+  componentWillMount () {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({user})
+    })
+  }
+
+  handleAuth () {
+    const provider = new firebase.auth.GoogleAuthProvider()
+
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} ha iniciado sesión`))
+      .catch(error => console.log(`Error ${error.code}:${error.message}`))
+  }
+
+  handleLogout () {
+    firebase.auth().signOut()
+      .then(result => console.log(`${result.user.email} ha salido de la sesión`))
+      .catch(error => console.log(`Error ${error.code}:${error.message}`))
+  }
+  renderLoginButton () {
+    // Sí el usuario ya esta logeado
+    if (this.state.user) {
+      return (
+        <div>
+          <img  width="250" src={this.state.user.photoURL} alt={this.state.user.displayName} />
+          <p> Hola {this.state.user.displayName} </p>
+          <button onClick={this.handleLogout}> Salir </button>
+        </div>
+      )
+    } else{
+    // Sí no lo está
+      return (<button onClick={this.handleAuth}> Login con Goole</button>
+      )
+    }
+  }
+  render () {
+    return (
+      <div className='App'>
+        <div className='App-header'>
+          <h2>My Social Network </h2>
+
+        </div>
+        <p className='App-intro'>
+          { this.renderLoginButton() }
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ mySocial/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import * as firebase from 'firebase'
+
+var config = {
+  apiKey: 'AIzaSyAtSswEBoMSKEmTCLYqtyshjlRbD8Ij5RU',
+  authDomain: 'red-social-ux.firebaseapp.com',
+  databaseURL: 'https://red-social-ux.firebaseio.com',
+  projectId: 'red-social-ux',
+  storageBucket: 'red-social-ux.appspot.com',
+  messagingSenderId: '517777747889'
+}
+firebase.initializeApp(config)
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
